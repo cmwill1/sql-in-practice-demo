c88b7508a7d2e0039b266d0fd27d0f694da80868
Christina Will <cmkupiec@me.com>
2022-07-24 00:31:03 -0400

    Finished Code-working

---
+++ server/controller.js
@@ -0,0 +1,67 @@
+require('dotenv').config()
+const Sequelize = require('sequelize')
+const { DATABASE_URL } = process.env
+
+const sequelize = new Sequelize(DATABASE_URL,{
+    dialect:'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized:false
+        }
+    }
+});
+
+const userId = 4;
+const clientId = 3;
+
+
+module.exports = {
+    getUserInfo: (req,res) => {
+        sequelize.query(`
+          select * from cc_users cu
+          join cc_clients cc
+          on cu.user_id = cc.user_id
+          where cu.user_id = ${userId}
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+    updateUserInfo: (req,res) => {
+      let { firstName , lastName, phoneNumber, email, address, city,state,zipCode} = req.body
+
+      sequelize.query(`
+          update cc_users set first_name = '${firstName}',
+          last_name = '${lastName}',
+          email = '${email}',
+          phone_number = ${phoneNumber}
+          where user_id = ${userId};
+          update cc_clients set address = '${address}',
+          city = '${city}',
+          state = '${state}',
+          zip_code = ${zipCode}
+          where client_id = ${clientId};
+      `)
+      .then(() => res.sendStatus(200))
+      .catch(err => console.log(err))
+    },
+    getUserAppt:(req,res) => {
+      sequelize.query(`
+        select * from cc_appointments
+        where client_id = ${clientId}
+        order by date desc
+      `)
+      .then(dbRes => res.status(200).send(dbRes[0]))
+      .catch(err => console.log(err))
+    },
+    requestAppointment:(req,res) => {
+      const { date, service } = req.body
+      sequelize.query(`
+        insert into cc_appointments(client_id,date,service_type,notes,approved,completed)
+        values(${clientId},'${date}','${service}','picky customer',false,false)
+        returning *
+      `)
+      .then(dbRes => res.status(200).send(dbRes[0]))
+      .catch(err => console.log(err))
+    }
+
+}
